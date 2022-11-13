c918dcbffc428f94b2c06caba53a1991a3a47478
Andreas Jonsson <ajdev8@gmail.com>
2022-11-13 20:36:08 +0100

    Remove GNU core utilities and other alternative utilities

--- brew.sh
+++ brew.sh
@@ -12,19 +12,19 @@ brew update
 brew upgrade
 
 # Save Homebrew’s installed location.
-BREW_PREFIX=$(brew --prefix)
+# BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+# brew install coreutils
+# ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+# brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+# brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+# brew install gnu-sed --with-default-names
 # Install a modern version of Bash.
 brew install bash
 # brew install bash-completion
@@ -40,9 +40,9 @@ fi;
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
+# brew install vim --with-override-system-vi
+# brew install grep
+# brew install openssh
 
 # Install other useful binaries.
 brew install git
